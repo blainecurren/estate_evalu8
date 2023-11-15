30a4645bc9c1011bff3a056cd306394763a54e0a
Blaine Curren <blaine.curren@gmail.com>
2023-11-15 16:08:21 -0600

    added topbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import "./App.css";
+import "./App.scss";
+import Topbar from "./components/topbar/Topbar";
 
 function App() {
   return (
     <>
-      <div>hello world</div>
+      <Topbar />
     </>
   );
 }
