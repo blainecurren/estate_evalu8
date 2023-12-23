2cc9bc3147aace0eedaac2616368db10e56d645e
Blaine Curren <blaine.curren@gmail.com>
2023-12-23 08:13:36 -0600

    added reactSlider

--- src/components/FormInput/FormInput.jsx
+++ src/components/FormInput/FormInput.jsx
@@ -1,79 +1,29 @@
-import { useState } from "react";
-import "./formInput.css";
+// import { useState } from "react";
+// import "./formInput.css";
 
-const [currentSlide, setCurrentSlide] = useState(0);
+// const FormInput = (props) => {
+//   const [focused, setFocused] = useState(false);
+//   const { label, errorMessage, onChange, id, ...inputProps } = props;
 
-const data = [
-  {
-    id: "1",
-    title: "Question 1",
-    text: "",
-  },
-  {
-    id: "2",
-    title: "Question 2",
-    text: "",
-  },
-  {
-    id: "3",
-    title: "Question 3",
-    text: "",
-  },
-  {
-    id: "4",
-    title: "Question 4",
-    text: "",
-  },
-  {
-    id: "5",
-    title: "Question 5",
-    text: "",
-  },
-  {
-    id: "6",
-    title: "Question 6",
-    text: "",
-  },
-  {
-    id: "7",
-    title: "Question 7",
-    text: "",
-  },
-  {
-    id: "8",
-    title: "Question 8",
-    text: "",
-  },
-  {
-    id: "9",
-    title: "Question 9",
-    text: "",
-  },
-];
+//   const handleFocus = (e) => {
+//     setFocused(true);
+//   };
 
-const FormInput = (props) => {
-  const [focused, setFocused] = useState(false);
-  const { label, errorMessage, onChange, id, ...inputProps } = props;
+//   return (
+//     <div className="formInput">
+//       <label>{label}</label>
+//       <input
+//         {...inputProps}
+//         onChange={onChange}
+//         onBlur={handleFocus}
+//         onFocus={() =>
+//           inputProps.name === "confirmPassword" && setFocused(true)
+//         }
+//         focused={focused.toString()}
+//       />
+//       <span>{errorMessage}</span>
+//     </div>
+//   );
+// };
 
-  const handleFocus = (e) => {
-    setFocused(true);
-  };
-
-  return (
-    <div className="formInput">
-      <label>{label}</label>
-      <input
-        {...inputProps}
-        onChange={onChange}
-        onBlur={handleFocus}
-        onFocus={() =>
-          inputProps.name === "confirmPassword" && setFocused(true)
-        }
-        focused={focused.toString()}
-      />
-      <span>{errorMessage}</span>
-    </div>
-  );
-};
-
-export default FormInput;
+// export default FormInput;
