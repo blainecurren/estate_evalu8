31fc12e2496be2f40a26b478b2e37371afecd7ad
Blaine Curren <blaine.curren@gmail.com>
2023-12-26 08:01:18 -0600

    added topbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 // TODO: Change answerOptions to input based responses
 import { useState } from "react";
 // import "./index.css";
-import FormInput from "./components/FormInput/FormInput";
-import ReactSlider from "./components/ReactSlider/ReactSlider";
+// import FormInput from "./components/FormInput/FormInput";
+import ReactSlider from "./components/reactSlider/ReactSlider";
+import Topbar from "./components/topbar/Topbar";
+import Menu from "./components/menu/Menu";
 import "./app.scss";
 
 const App = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="app">
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <ReactSlider />
       </div>
