a3d472eaf03b2c42e5f733d74ddf5b8eb7011978
Blaine Curren <blaine.curren@gmail.com>
2024-01-01 15:47:25 -0600

    restyled slider'

--- client/src/components/ReactSlider/ReactSlider.jsx
+++ client/src/components/ReactSlider/ReactSlider.jsx
@@ -7,63 +7,53 @@ export default function ReactSlider() {
   const data = [
     {
       id: "1",
-      icon: "./assets/mobile.png",
       title: "Bedrooms",
       question: "How many bedrooms?",
     },
 
     {
       id: "2",
-      icon: "./assets/writing.png",
       title: "Bathrooms",
       question: "How many bathrooms?",
     },
 
     {
       id: "3",
-      icon: "./assets/globe.png",
       title: "Sq. Ft.",
       question: "What is the square footage?",
     },
     {
       id: "4",
-      icon: "./assets/globe.png",
       title: "Rent",
       question: "What is the rent?",
     },
     {
       id: "5",
-      icon: "./assets/globe.png",
       title: "Vacancies",
       question: "How many vacancies if any?",
     },
     {
       id: "6",
-      icon: "./assets/globe.png",
       title: "Repairs",
       question: "What is the estimated cost of repairs?",
     },
     {
       id: "7",
-      icon: "./assets/globe.png",
       title: "Utilities",
       question: "Estimated cost of monthly utilities?",
     },
     {
       id: "8",
-      icon: "./assets/globe.png",
       title: "Taxes",
       question: "What are the estimated taxes?",
     },
     {
       id: "9",
-      icon: "./assets/globe.png",
       title: "Insurance",
       question: "What is the estimated insurance?",
     },
     {
       id: "10",
-      icon: "./assets/globe.png",
       title: "Replacement Reserve",
       question: "What is your replacement reserve?",
     },
@@ -99,25 +89,21 @@ export default function ReactSlider() {
                     <img src={d.icon} alt="" />
                   </div>
                   <h2>{d.title}</h2>
-                  <p>{d.question}</p>
-                  <form>
-                    <input
-                      type="text"
-                      className="input"
-                      value={answer}
-                      onChange={(e) => setAnswer(e.target.value)}
-                    />
-                    <button className="submit" onClick={handleSubmit}>
-                      Submit
-                    </button>
-                  </form>
                 </div>
               </div>
               <div className="right">
-                <img
-                  src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930"
-                  alt=""
-                />
+                <p>{d.question}</p>
+                <form>
+                  <input
+                    type="text"
+                    className="input"
+                    value={answer}
+                    onChange={(e) => setAnswer(e.target.value)}
+                  />
+                  <button className="submit" onClick={handleSubmit}>
+                    Submit
+                  </button>
+                </form>
               </div>
             </div>
           </div>
